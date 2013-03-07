8cf0740a5d37609dc1b38dc8fd34da2977945b2a
comki1 <comki1@comki1-PC>
2013-03-07 18:35:39 +0900

    파일 수정 및 추가

--- index.php
+++ index.php
@@ -1,2 +1,46 @@
+<?php
+$t1 = time()-(60*60*24*3);
+$t2 = time()-(60*60*24*2);
+$t3 = time()-(60*60*24*1);
+$t4 = time();
 
-hello!!
+echo date('Y-n-d H:i:s',$t1);
+echo ' '.$t4;
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<!-- 
+<script type="text/javascript" src="js/flotr2.min.js"></script>
+ -->
+
+<style type="text/css">
+#cv { width:600px;height:400px;margin:20px auto; }
+</style>
+<script type="text/javascript" src="http://apptable.net/exam/lib/flotr2.min.js"></script>
+
+</head>
+<body>
+	<a href="http://apptable.net/exam/lib/flotr2.min.js"> asdfasdfs</a>
+	<div id="cv"></div>
+	<script>
+(function basic_time(container){
+	var
+	 d1 = [[<?=$t1?>,3],[<?=$t2?>,8],[<?=$t3?>,20],[<?=$t4?>,200]],
+	 i, graph;
+	graph = Flotr.draw(container,[d1],{
+		xaxis:{
+			mode:'time',
+			labelIsAngle:45
+		},
+		selection:{
+			mode:'x'
+		},
+		grid:{
+			minorVerticalLines:true
+		}
+	});
+})(document.getElementById("cv"));
+</script>
+</body>
+</html>
